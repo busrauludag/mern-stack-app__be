6ff044caab1f8dc61e0b7a45dac3359a231870a3
busrauludag <busrauludag13@gmail.com>
2019-05-08 22:17:57 +0300

    express route added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const todoRoutes = express.Router();
 const PORT = 4000;
 
 let Todo = require('./todo.model');
@@ -15,6 +16,8 @@ const connection = mongoose.connection;
 connection.once('open', function(){
   console.log('MongoDB database connection established successfully.');
 });
+
+app.use('/todos', todoRoutes);
 
 app.listen(PORT, function(){
   console.log(`Server is runnig on Port: ${PORT}`);
